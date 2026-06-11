93e71c03a5d7fbec82e1eac061ec3cdb3ad3f6ae
John Muthomi <michenijohn20@gmail.com>
2026-06-11 10:42:03 +0300

    Add: title,authot,genre,due date,member attributes

--- models/book.py
+++ models/book.py
@@ -1,1 +1,12 @@
-class Book: 
+class Book:
+
+    book_count = 0
+
+    def __init__(self, title, author, member, due_date="", genre="", reviews=None):
+        self.title = title
+        self.author = author
+        self.member = member
+        self.due_date = due_date
+        self.genre = genre
+        self.reviews = reviews or []
+        Book.book_count += 1
