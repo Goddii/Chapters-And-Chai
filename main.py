2bd4c834d423d32749e1a20219dba77f193ecd74
Goddii <otienogodwin23@gmail.com>
2026-06-11 16:48:46 +0300

    feat(main): add add_review cli

--- main.py
+++ main.py
@@ -28,3 +28,12 @@ def buid_parser():
     list_book_parser = subparser.add_parser("list-books", help="List books")
     list_books_parser.add_argument("--member", default="")
     list_books_parser.set_defaults(func=list_books)
+
+    add_review_parser = subparsers.add_parser('add-review', help="Add a review")
+    add_review_parser.add_argument("--book", required=True)
+    add_review_parser.add_argument("--rating", required=True, type=int)
+    add_review_parser.add_argument("--notes", default='')
+    add_review_parser.add_argument("--status", default="reading", choices=Review.allow_statuses)
+    add_review_parser.add_argument("--assigned-to", required=True)
+    add_review_parser.add_argument("--member", required=True)
+    add_review_parser.set_defaults(func=add_reviews)
