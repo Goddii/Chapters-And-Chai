87f0fbbe3b723ef009b94e267d7d07f34770d9d2
Goddii <otienogodwin23@gmail.com>
2026-06-11 12:41:57 +0300

    feat: add classes from models

--- lib/functions.py
+++ lib/functions.py
@@ -6,3 +6,10 @@ from dateutils import parser as date_parser
 from rich.console import Console
 from rich.table import Table
 
+from models.book import Book
+from models.member import Member
+from models.review import Review
+
+
+
+
