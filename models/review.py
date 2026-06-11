21094707aa612a367f997f9e316964b7a5062987
teresiawangondu <arwatess@gmail.com>
2026-06-11 14:51:18 +0300

    add rating setter

--- models/review.py
+++ models/review.py
@@ -8,3 +8,13 @@ class Review:
         self.notes = notes
         self.status = status
         self.assigned_to = assigned_to
+ @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, value):
+        value = int(value)
+        if value < 1 or value > 5:
+            raise ValueError("Rating must be between 1 and 5.")
+        self._rating = value
