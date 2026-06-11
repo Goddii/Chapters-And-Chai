b1a21657d4a983888237e1334694aaa571cc0682
Goddii <otienogodwin23@gmail.com>
2026-06-11 12:40:26 +0300

    feat: add rich and dateutils library

--- lib/functions.py
+++ lib/functions.py
@@ -1,3 +1,8 @@
 from datetime import date, timedelta
 import json
 import pathlib import Path
+
+from dateutils import parser as date_parser
+from rich.console import Console
+from rich.table import Table
+
