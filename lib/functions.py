2331aca21280da093ee9f6af297b2a2946c39d95
Goddii <otienogodwin23@gmail.com>
2026-06-11 12:47:36 +0300

    feat: add function to save data to json files

--- lib/functions.py
+++ lib/functions.py
@@ -15,5 +15,12 @@ MEMBER_FILE = DATA_DIR / "members.json"
 BOOK_FILE = DATA_DIR / "books.json"
 
 
+def save_json(file_path, data):
+    """ save data to a json file"""
+    DATA_DIR.mkdir(exist_ok=True)
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+
 
 
